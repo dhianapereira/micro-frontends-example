5605bb5d31ed2310847e8e49ebcde026d9ffad7e
dhianapereira <dhianaslv@outlook.com>
2024-04-17 13:45:57 -0300

    feat(MA_login): Implement micro app login

--- micro_apps/micro_app_login/lib/micro_app_login.dart
+++ micro_apps/micro_app_login/lib/micro_app_login.dart
@@ -1,7 +1,3 @@
 library micro_app_login;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src//configs/micro_app.dart';
---
+++ micro_apps/micro_app_login/lib/src/configs/micro_app.dart
@@ -0,0 +1,15 @@
+import 'package:core/core.dart';
+import 'package:micro_app_login/src/configs/constants.dart';
+import 'package:micro_app_login/src/login_page.dart';
+
+class MicroAppLogin implements MicroApp {
+  @override
+  String get microAppName => Constants.microAppName;
+
+  @override
+  Map<String, WidgetBuilderArgs> get routes {
+    return {
+      '/login': (context, args) => const LoginPage(),
+    };
+  }
+}
